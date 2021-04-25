562f92ccadce432bdf2805b181992f76a0d7d148
brunonom <bnom2013@gmail.com>
2021-04-25 05:46:11 -0300

    too many improvements

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 let ggraph = new graph();
 let canvas;
 let inputbox;
+let labelbuffer;
+let dropdownmenu;
 let runbutton;
-let running = false;
+let clearbutton;
 function setup() {
     setcanvas();
-    seteventlisteners();
     setsidebar();
 
     ellipseMode(RADIUS);
@@ -18,19 +19,22 @@ function setup() {
 }
 
 function draw() {
-    background(0);
+    background("#1f1f1f");
 
-    if (!running) {
+    noFill(); stroke(100); strokeWeight(1);
+    line(windowWidth * 0.17, windowHeight * 0.05, windowWidth * 0.17, windowHeight * 0.95);
+
+    if (!ggraph.locked) {
         if (ggraph.selectedelement == 0) {
             inputbox.value = ggraph.nodes[ggraph.selectedindex].label;
         } else if (ggraph.selectedelement == 1) {
             inputbox.value = ggraph.edges[ggraph.selectedindex].label;
         } else {
-            inputbox.value = "edit label";
+            inputbox.value = "";
         }
 
         if (ggraph.selectedelement == 0 && mouseIsPressed) {
-            stroke(240, 100, 50); strokeWeight(1);
+            stroke(0, 95, 60); strokeWeight(1);
             let dx = mouseX;
             let dy = mouseY;
             let nodemouseisin = -1;
@@ -67,13 +71,6 @@ function draw() {
     ggraph.draw();
 }
 
-function keyPressed() {
-    if (keyCode === DELETE) {
-        ggraph.removenode();
-        ggraph.removeedge();
-    }
-}
-
 function setcanvas() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.elt.style.zIndex = -1;
@@ -81,20 +78,33 @@ function setcanvas() {
     const x = (windowWidth - width) / 2;
     const y = (windowHeight - height) / 2;
     canvas.position(x, y);
-}
 
-function seteventlisteners() {
-    canvas.doubleClicked(() => {
-        if (!running) {
+    window.addEventListener("keydown", (event) => {
+        if (event.code == "Delete") {
+            ggraph.removeselected();
+        }
+    });
+
+    canvas.elt.addEventListener("dblclick", () => {
+        if (mouseX <= windowWidth * 0.17) return;
+        if (!ggraph.locked) {
             if (ggraph.selectedelement == -1) {
                 ggraph.addnode(mouseX, mouseY, 20);
-                // print("added node");
             }
         }
     });
 
-    canvas.mousePressed(() => {
-        if (!running) {
+    canvas.elt.addEventListener("click", () => {
+        if (mouseX <= windowWidth * 0.17) return;
+        ggraph.select(mouseX, mouseY);
+        if (ggraph.selectedelement != -1) {
+            inputbox.focus();
+        }
+    });
+
+    canvas.elt.addEventListener("mousedown", () => {
+        if (mouseX <= windowWidth * 0.17) return;
+        if (!ggraph.locked) {
             ggraph.select(mouseX, mouseY);
             if (ggraph.selectedelement == 1) {
                 if (keyIsDown(CONTROL)) {
@@ -104,13 +114,14 @@ function seteventlisteners() {
         }
     });
 
-    canvas.mouseReleased(() => {
-        if (!running) {
+    canvas.elt.addEventListener("mouseup", () => {
+        if (mouseX <= windowWidth * 0.17) return;
+        if (!ggraph.locked) {
             if (keyIsDown(SHIFT)) {
                 for (let i = ggraph.nodes.length - 1; i >= 0; i--) {
                     if (ggraph.nodes[i].contains(mouseX, mouseY)) {
                         ggraph.addegde(ggraph.nodes[ggraph.selectedindex], ggraph.nodes[i]);
-                        // print("added edge");
+                        // console.log("added edge");
                         break;
                     }
                 }
@@ -118,7 +129,7 @@ function seteventlisteners() {
                 for (let i = ggraph.nodes.length - 1; i >= 0; i--) {
                     if (ggraph.nodes[i].contains(mouseX, mouseY)) {
                         ggraph.adddirectedegde(ggraph.nodes[ggraph.selectedindex], ggraph.nodes[i]);
-                        // print("added edge");
+                        // console.log("added edge");
                         break;
                     }
                 }
@@ -126,8 +137,9 @@ function seteventlisteners() {
         }
     });
 
-    canvas.mouseMoved(() => {
-        if (!running) {
+    canvas.elt.addEventListener("mousemove", () => {
+        if (mouseX <= windowWidth * 0.17) return;
+        if (!ggraph.locked) {
             if (mouseIsPressed) {
                 if (ggraph.selectedelement == 0) {
                     if (!keyIsDown(SHIFT) && !keyIsDown(CONTROL)) {
@@ -139,19 +151,48 @@ function seteventlisteners() {
     });
 }
 
+function windowResized() {
+    canvas.resize(windowWidth, windowHeight);
+    const x = (windowWidth - width) / 2;
+    const y = (windowHeight - height) / 2;
+    canvas.position(x, y);
+    draw();
+}
+
 function setsidebar() {
     let sidebardiv = document.createElement("div");
+    document.body.appendChild(sidebardiv);
     sidebardiv.style.position = "absolute";
-    sidebardiv.style.top = "20px";
+    sidebardiv.style.top = "5%";
+    sidebardiv.style.bottom = "5%";
     sidebardiv.style.left = "20px";
+    sidebardiv.style.right = "85%";
     sidebardiv.style.zIndex = 1;
+    sidebardiv.style.flexDirection = "column";
+
+    //instructions
+    let instructions = document.createElement("p");
+    sidebardiv.appendChild(instructions);
+    instructions.style.color = "white";
+    instructions.style.userSelect = "none";
+    instructions.innerHTML = `
+    CONTROLS: <br/> <br/>
+    - add node: double click <br/> <br/>
+    - move node: click + drag <br/> <br/>
+    - add undirected edge: shift + drag <br/> <br/>
+    - add directed edge: ctrl + drag <br/> <br/>
+    - toggle edge direction: crtl + click <br/> <br/>
+    - select element: click <br/> <br/>
+    - edit label: select + type <br/> <br/>
+    - delete element: select + DEL
+    `;
 
     //inputbox
     inputbox = document.createElement("input");
+    sidebardiv.appendChild(inputbox);
     inputbox.type = "text";
     inputbox.id = "inputbox";
-    inputbox.value = "edit label";
-    inputbox.style.width = 150;
+    inputbox.placeholder = "edit label";
     inputbox.addEventListener("input", (e) => {
         if (ggraph.selectedelement == 0) {
             ggraph.nodes[ggraph.selectedindex].setlabel(inputbox.value);
@@ -159,223 +200,45 @@ function setsidebar() {
             ggraph.edges[ggraph.selectedindex].setlabel(inputbox.value);
         }
     });
-    sidebardiv.appendChild(inputbox);
+
+    //algorithmselect
+    const algorithmselect = document.createElement("select");
+    sidebardiv.appendChild(algorithmselect);
+    algorithmselect.addEventListener("change", (e) => {
+        runbutton.addEventListener("click", (e) => {
+            switch (algorithmselect.value) {
+                case "topologicalsort":
+                    if (!isempty() && isdirected() && !hascycle()) {
+                        topologicalsort();
+                    }
+                    break;
+            }
+        });
+    });
+    const placeholderoption = document.createElement("option");
+    placeholderoption.value = "";
+    placeholderoption.innerHTML = "select algorithm";
+    placeholderoption.disabled = "";
+    placeholderoption.selected = "";
+    algorithmselect.appendChild(placeholderoption);
+    const topologicalsortoption = document.createElement("option");
+    topologicalsortoption.value = "topologicalsort";
+    topologicalsortoption.innerHTML = "topological sort";
+    algorithmselect.appendChild(topologicalsortoption);
 
     //runbutton
     runbutton = document.createElement("button");
-    runbutton.innerHTML = "run";
-    runbutton.addEventListener("click", (e) => {
-        if (!isempty() && isdirected() && !hascycle()) {
-            topologicalsort();
-        }
-    });
     sidebardiv.appendChild(runbutton);
-    document.body.appendChild(sidebardiv);
-}
+    runbutton.innerHTML = "run algorithm";
 
-function isempty() {
-    return ggraph.nodes.length == 0;
-}
-
-function isdirected() {
-    for (let i = 0; i < ggraph.edges.length; i++) {
-        if (ggraph.edges[i].direction == 2) {
-            print("not a directed graph");
-            return false;
-        }
-    }
-    return true;
-}
-
-function hascycle() {
-    let adjmatrix = [];
-    for (let i = 0; i < ggraph.nodes.length; i++) {
-        const u = ggraph.nodes[i];
-        adjmatrix.push([i]);
-        for (let j = 0; j < ggraph.edges.length; j++) {
-            const e = ggraph.edges[j];
-            if (e.direction == 0 && e.u == u) {
-                adjmatrix[i].push(ggraph.nodes.indexOf(e.v));
-            } else if (e.direction == 1 && e.v == u) {
-                adjmatrix[i].push(ggraph.nodes.indexOf(e.u));
-            }
-        }
-    }
-
-    let stack = [];
-    let visited = new Array(ggraph.nodes.length);
-
-    function hascycledfs() {
-        const childrenstacktop = adjmatrix[stack[stack.length - 1]];
-        for (let i = 1; i < childrenstacktop.length; i++) {
-            const child = childrenstacktop[i];
-            if (!visited[child]) {
-                visited[child] = true;
-                stack.push(child);
-                if (hascycledfs()) {
-                    return true;
-                }
-            } else {
-                for (let j = 0; j < stack.length; j++) {
-                    if (child == stack[j]) return true;
-                }
-                return false;
-            }
-        }
-        stack.pop();
-        return false;
-    }
-
-    for (let i = 0; i < ggraph.nodes.length; i++) {
-        for (let j = 0; j < visited.length; j++) {
-            visited[j] = false;
-        }
-        stack.push(i);
-        visited[i] = true;
-        if (hascycledfs()) {
-            print("graph contains cycle");
-            return true;
-        }
-    }
-}
-
-function componentreps() {
-    let adjmatrix = new Array(ggraph.nodes.length);
-    for (let i = 0; i < adjmatrix.length; i++) {
-        adjmatrix[i] = [i];
-    }
-    for (let i = 0; i < adjmatrix.length; i++) {
-        const u = ggraph.nodes[i];
-        for (let j = 0; j < ggraph.edges.length; j++) {
-            const e = ggraph.edges[j];
-            if (e.u == u) {
-                adjmatrix[i].push(ggraph.nodes.indexOf(e.v));
-                adjmatrix[ggraph.nodes.indexOf(e.v)].push(i);
-            }
-        }
-    }
-    // print("adjmat", adjmatrix)
-
-
-    let visited = new Array(adjmatrix.length);
-    for (let i = 0; i < visited.length; i++) {
-        visited[i] = false;
-    }
-    let representatives = [];
-    let queue = [];
-
-    function componentrepsbfs() {
-        while (queue.length != 0) {
-            // print("queue", queue)
-            // print("vis", visited)
-            const connectionsqueuefront = adjmatrix[queue[0]];
-            // print("connectionsqueuefront", connectionsqueuefront)
-            for (let i = 1; i < connectionsqueuefront.length; i++) {
-                // print("connectionsqueuefront i", connectionsqueuefront[i])
-                if (!visited[connectionsqueuefront[i]]) {
-                    visited[connectionsqueuefront[i]] = true;
-                    queue.push(connectionsqueuefront[i]);
-                    // print("visited and pushed", i)
-                }
-            }
-            queue.shift();
-        }
-    }
-
-    for (let i = 0; i < adjmatrix.length; i++) {
-        // print("trying", i)
-        if (!visited[i]) {
-            // print("visited and pushed", i)
-            visited[i] = true;
-            representatives.push(i);
-            queue.push(i);
-            componentrepsbfs();
-        }
-    }
-
-    return representatives;
-}
-
-async function topologicalsort() {
-    running = true;
-    ggraph.unselect();
-    let adjmatrix = [];
-    for (let i = 0; i < ggraph.nodes.length; i++) {
-        const v = ggraph.nodes[i];
-        adjmatrix.push([i]);
-        for (let j = 0; j < ggraph.edges.length; j++) {
-            const e = ggraph.edges[j];
-            if (e.direction == 0 && e.v == v) {
-                adjmatrix[i].push(ggraph.nodes.indexOf(e.u));
-            } else if (e.direction == 1 && e.u == v) {
-                adjmatrix[i].push(ggraph.nodes.indexOf(e.v));
-            }
-        }
-    }
-
-    // let stack = [];
-    // let visited = new Array(adjmatrix.length);
-    // for(let j=0; j<visited.length; j++){
-    // 	visited[j] = false;
-    // }
-    // let nodelabelcounter = 1;
-
-    // function topologicalsortdfs(level){
-    // 	const parentsstacktop = adjmatrix[stack[stack.length - 1]];
-    // 	for(let i=1; i<parentsstacktop.length; i++){
-    // 		const parent = parentsstacktop[i];
-    // 		if(!visited[parent]){
-    // 			visited[parent] = true;
-    // 			stack.push(parent);
-    // 			topologicalsortdfs(level + 1);
-    // 		}
-    // 	}
-    // 	ggraph.nodes[parentsstacktop[0]].label = level;
-    // 	nodelabelcounter++;
-    // 	stack.pop();
-    // }
-
-    let order = [];
-    let orphans = [];
-    for (let i = 0; i < adjmatrix.length; i++) {
-        if (adjmatrix[i].length == 1) {
-            orphans.push(i);
-        }
-    }
-
-    while (orphans.length > 0) {
-        order.push(orphans[0]);
-        for (let i = 0; i < adjmatrix.length; i++) {
-            const pos = adjmatrix[i].indexOf(orphans[0]);
-            if (pos != -1) {
-                adjmatrix[i].splice(pos, 1);
-                if (adjmatrix[i].length == 1) {
-                    orphans.push(adjmatrix[i][0]);
-                }
-            }
-        }
-        orphans.shift();
-    }
-
-    for (let i = 0; i < ggraph.nodes.length; i++) {
-        ggraph.nodes[i].label = "";
-        ggraph.nodes[i].hue = 0;
-        ggraph.nodes[i].sorted = false;
-    }
-
-    // print(order)
-    for (let i = 0; i < order.length; i++) {
-        ggraph.nodes[order[i]].label = i;
-        ggraph.nodes[order[i]].sorted = true;
-        await new Promise(r => setTimeout(r, 1000));
-    }
-
-    // let representatives = componentreps();
-    // print("reps", representatives);
-    // for(let i=0; i<representatives.length; i++){
-    //     stack.push(representatives[i]);
-    //  visited[i] = true;
-    //     topologicalsortdfs(0);
-    // }
-    running = false;
+    //clearbutton
+    clearbutton = document.createElement("button");
+    sidebardiv.appendChild(clearbutton);
+    clearbutton.innerHTML = "clear all nodes";
+    clearbutton.style.position = "absolute";
+    clearbutton.style.bottom = "0px";
+    clearbutton.style.width = "100%";
+    clearbutton.addEventListener("click", (e) => {
+        ggraph.clear();
+    });
 }
--- objects.js
+++ objects.js
@@ -10,31 +10,24 @@ function lineangle(xa, ya, xb, yb) {
     return Math.atan2((ya - yb), (xa - xb));
 }
 
-let nodeidcounter = 0;
-let edgeidcounter = 0;
-
 class node {
     constructor(x, y, r) {
         this.x = x;
         this.y = y;
         this.r = r;
-        this.hue = 0;
-        this.saturation = 100;
-        this.lightness = 50;
+
+        this.hue = 120;
+        this.saturation = 70;
+        this.lightness = 60;
         this.label = "";
-        this.sorted = false;
     }
 
     draw() {
         fill(this.hue, this.saturation, this.lightness); noStroke();
         ellipse(this.x, this.y, this.r, this.r);
 
-        fill(100);
+        fill(90); stroke(0);
         text(this.label, this.x, this.y);
-
-        if (this.sorted) {
-            this.hue = 300;
-        }
     }
 
     clear() {
@@ -62,11 +55,12 @@ class edge {
     constructor(u, v) {
         this.u = u;
         this.v = v;
-        this.hue = 240;
-        this.saturation = 100;
-        this.lightness = 50;
-        this.label = "";
         this.direction = 2; // |  0: u -> v  |  1: u <- v  |  2: u - v  |
+
+        this.hue = 0;
+        this.saturation = 95;
+        this.lightness = 60;
+        this.label = "";
     }
 
     draw() {
@@ -90,8 +84,15 @@ class edge {
             line(borderpoint.x, borderpoint.y, borderpoint.x - Math.cos(angle - Math.PI / 6) * 15, borderpoint.y - Math.sin(angle - Math.PI / 6) * 15);
         }
 
-        fill(100); noStroke(100);
+        fill(90); stroke(0);
         text(this.label, min(this.u.x, this.v.x) + abs(this.u.x - this.v.x) / 2, min(this.u.y, this.v.y) + abs(this.u.y - this.v.y) / 2);
+    }
+
+    clear() {
+        this.hue = 240;
+        this.saturation = 100;
+        this.lightness = 50;
+        this.label = "";
     }
 
     toggledirection() {
@@ -111,42 +112,42 @@ class graph {
     constructor() {
         this.nodes = [];
         this.edges = [];
-        this.selectedelement = -1; // |  -1: none  |  0: node  |  1: edge  |
-        this.selectedindex = -1;
+        this.selectedelement = -1; // type of the selected element |  -1: none  |  0: node  |  1: edge  |
+        this.selectedindex = -1; // index of the selected element in the according type's array
         this.selectedhue = 0;
     }
 
     addnode(x, y, r) {
         let n = new node(x, y, r);
         this.nodes.push(n);
-        // print(this)
+        // console.log(this)
     }
 
     addegde(u, v) {
         if (u != v) {
             for (let i = 0; i < this.edges.length; i++) {
                 if (this.edges[i].u == u && this.edges[i].v == v) {
-                    print("already added edge");
+                    console.log("already added edge");
                     return;
                 } else if (this.edges[i].u == v && this.edges[i].v == u) {
-                    print("already added edge");
+                    console.log("already added edge");
                     return;
                 }
             }
             let e = new edge(u, v);
             this.edges.push(e);
         }
-        // print(this)
+        // console.log(this)
     }
 
     adddirectedegde(u, v) {
         if (u != v) {
             for (let i = 0; i < this.edges.length; i++) {
                 if (this.edges[i].u == u && this.edges[i].v == v) {
-                    print("already added edge");
+                    console.log("already added edge");
                     return;
                 } else if (this.edges[i].u == v && this.edges[i].v == u) {
-                    print("already added edge");
+                    console.log("already added edge");
                     return;
                 }
             }
@@ -154,32 +155,34 @@ class graph {
             e.direction = 0;
             this.edges.push(e);
         }
-        // print(this);
+        // console.log(this);
     }
 
     removenode() {
-        if (this.selectednode != -1) {
-            for (let i = 0; i < this.edges.length; i++) {
-                if (this.edges[i].u == this.nodes[this.selectednode] || this.edges[i].v == this.nodes[this.selectednode]) {
-                    this.edges.splice(i, 1);
-                    i--;
-                }
+        for (let i = 0; i < this.edges.length; i++) {
+            if (this.edges[i].u == this.nodes[this.selectedindex] || this.edges[i].v == this.nodes[this.selectedindex]) {
+                this.edges.splice(i, 1);
+                i--;
             }
-            this.nodes.splice(this.selectednode, 1);
-            this.selectednode = -1;
         }
-        // print(this)
+        this.nodes.splice(this.selectedindex, 1);
+        this.unselect();
+        // console.log(this)
     }
 
     removeedge() {
-        if (this.selectededge != -1) {
-            this.edges.splice(this.selectededge, 1);
-            this.selectededge = -1;
-        }
-        // print(this)
+        this.edges.splice(this.selectedindex, 1);
+        this.unselect();
+        // console.log(this)
+    }
+
+    removeselected() {
+        if (this.selectedelement == 0) this.removenode();
+        else if (this.selectedelement == 1) this.removeedge();
     }
 
     select(x, y) {
+        // nodes need to be checked first because they are visually on top of edges
         for (let i = this.nodes.length - 1; i >= 0; i--) {
             if (this.nodes[i].contains(x, y)) {
                 this.selectedelement = 0;
@@ -194,6 +197,7 @@ class graph {
                 return;
             }
         }
+        this.unselect();
     }
 
     unselect() {
@@ -202,20 +206,77 @@ class graph {
     }
 
     draw() {
+        // edges need to be drawn first so that nodes are drawn on top of them
         for (let i = 0; i < this.edges.length; i++) {
             this.edges[i].draw();
             if (this.selectedindex == i && this.selectedelement == 1) {
-                noFill(); stroke(this.selectedhue, 100, 50); strokeWeight(3);
+                noFill(); stroke(100); strokeWeight(2);
                 ellipse((this.edges[i].u.x + this.edges[i].v.x) / 2, (this.edges[i].u.y + this.edges[i].v.y) / 2, this.nodes[0].r, this.nodes[0].r);
             }
         }
         for (let i = 0; i < this.nodes.length; i++) {
             this.nodes[i].draw();
             if (this.selectedindex == i && this.selectedelement == 0) {
-                noFill(); stroke(this.selectedhue, 100, 50); strokeWeight(3);
+                noFill(); stroke(100); strokeWeight(2);
                 ellipse(this.nodes[i].x, this.nodes[i].y, this.nodes[i].r, this.nodes[i].r);
             }
         }
         this.selectedhue = (this.selectedhue + 5) % 360;
+    }
+
+    clear() {
+        this.unselect();
+        this.nodes = [];
+        this.edges = [];
+    }
+
+    get_adjacency_matrix() {
+        let adjmatrix = [];
+        for (let i = 0; i < this.nodes.length; i++) {
+            adjmatrix.push([]);
+            for (let j = 0; j < this.nodes.length; j++) {
+                adjmatrix[i].push(0);
+            }
+        }
+        for (let i = 0; i < this.nodes.length; i++) {
+            const u = this.nodes[i];
+            for (let j = 0; j < this.nodes.length; j++) {
+                const v = this.nodes[j];
+                for (let k = 0; k < this.edges.length; k++) {
+                    if (this.edges[k].u == u && this.edges[k].v == v) {
+                        switch (this.edges[k].direction) {
+                            case 0:
+                                adjmatrix[i][j] = 1;
+                                adjmatrix[j][i] = -1;
+                                break;
+                            case 1:
+                                adjmatrix[i][j] = -1;
+                                adjmatrix[j][i] = 1;
+                                break;
+                            case 2:
+                                adjmatrix[i][j] = 2;
+                                adjmatrix[j][i] = 2;
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+        return adjmatrix;
+    }
+
+    get_adjacency_list() {
+        let adjlist = [];
+        for (let i = 0; i < this.nodes.length; i++) {
+            const u = this.nodes[i];
+            adjlist.push([]);
+            for (let j = 0; j < this.edges.length; j++) {
+                const e = this.edges[j];
+                if (e.direction == 0 && e.u == u) {
+                    adjlist[i].push(this.nodes.indexOf(e.v));
+                }
+            }
+        }
+        return adjlist;
     }
 }
