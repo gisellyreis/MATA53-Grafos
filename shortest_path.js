c420b979945eba796583cafcb68507e15a874651
viniciusth <viniciustht@gmail.com>
2021-05-29 16:29:01 -0300

    Algoritmos de shortest path

--- shortest_path.js
+++ shortest_path.js
@@ -1,208 +1,104 @@
-// retorna todos os vizinhos do nó u
-function neighbor(u) {
-    var adjmatrix = ggraph.get_adjacency_matrix();
+async function Johnsons(source, target) {
 
-    var neighbors = [];
-    for (let i = 0; i < ggraph.nodes.length; i++) {
-        if (adjmatrix[u][i] != 0) {
-            neighbors.push(i);
+    // step 1 and 2: use bellman ford to calculate h(x) for every x in the graph
+    // h(x) is the minimum value i can get for starting in any node in the graph
+    // and making a path to x. 
+    // to achieve this we create a fictional node Q connected to every node in the graph
+    // with distance 0.
+    h = await Bellman();
+    if(!h){
+        return;
+    }
+    // reweight the edges using h:
+    for(let i=0; i < ggraph.edges.length; i++){
+        let u = ggraph.edges[i].uidx;
+        let v = ggraph.edges[i].vidx;
+        let c = parseInt(ggraph.edges[i].label);
+        c += h[u] - h[v];
+        ggraph.edges[i].label = c.toString();
+    }
+
+    // finally: run dijkstra to find the shortest path between these two nodes.
+    await Dijkstra(source, target);
+}
+async function Bellman() {
+    let h = [], from = [];
+    let n = ggraph.nodes.length;
+    for(let i=0; i < n; i++){
+        h.push(0);
+        from.push(0);
+    }
+    // h is the distance from Q to every node in the graph.
+    // since it is connected with everyone with distance 0,
+    // we can start h with N zeroes to represent the start
+    // of the bellman-ford algorithm.
+    let m = ggraph.edges.length;
+    for(let i=0; i < n-1; i++){
+        for(let j=0; j < m; j++){
+            let u = ggraph.edges[j].uidx;
+            let v = ggraph.edges[j].vidx;
+            let c = parseInt(ggraph.edges[j].label);
+            if(h[v] > h[u] + c){
+                h[v] = h[u] + c;
+                from[v] = u;
+            }
         }
     }
-    return neighbors;
-}
-
-
-async function Johnsons(source, target) {
-   
-    Bellman(source, target);
-
+    for(let j=0; j < m; j++){ // if at the nth iteration there is a relaxation, there is a negative cycle.
+        let u = ggraph.edges[j].uidx;
+        let v = ggraph.edges[j].vidx;
+        let c = parseInt(ggraph.edges[j].label);
+        if(h[v] > h[u] + c){
+            used = {};
+            let at = v;
+            while(true){
+                ggraph.edges[ggraph.get_edge_index(at, from[at])].hue = 240;
+                used[at] = 1;
+                if(used.hasOwnProperty(from[at])){
+                    break;
+                }
+                at = from[at];
+            }
+            warn("Existe um ciclo negativo no grafo.");
+            return null;
+        }
+    }
+    return h;
 }
 
 async function Dijkstra(source, target) {
-
-    var visitados = [];
-    var predecessor = [];
-
-    for (let i = 0; i < ggraph.nodes.length; i++) {
-        ggraph.nodes[i].weight = Infinity;
-        predecessor[i] = -1;
-        visitados[i] = false;
+    let n = ggraph.nodes.length;
+    distance = [];
+    vis = [];
+    for(let i=0;i<n;i++){
+        distance.push(Infinity);
+        vis.push(false);
     }
-
-    ggraph.nodes[0].weight = 0;
-
-
-    for (let i = 0; i < ggraph.nodes.length; i++) {
-        let v = -1;
-        for (let j = 0; j < ggraph.nodes.length; j++) {
-            if (!visitados[j] && (v == -1 || ggraph.nodes[j].weight < ggraph.nodes[v].weight)) {
-                v = j;
+    let adj = ggraph.get_adjacency_list();
+    distance[source] = 0;
+    for(let it=0; it < n; it++){
+        let u = -1;
+        for(let v=0; v < n; v++){
+            if(!vis[v] && (u == -1 || distance[v] < distance[u])){
+                u = v;
             }
-
         }
-
-        if (ggraph.nodes[v].weight == Infinity) {
+        if(distance[u] == Infinity){
             break;
         }
-
-        visitados[v] = true;
-
-        var neighbors = neighbor(v);
-        var edg;
-        for (let k = 0; k < neighbors.length; k++) {
-            edg = ggraph.get_edge_index(v, neighbors[k]);
-            if ((ggraph.nodes[v].weight + ggraph.edges[edg].weight) < ggraph.nodes[neighbors[k]].weight) {
-                ggraph.nodes[neighbors[k]].weight = ggraph.nodes[v].weight + ggraph.edges[edg].weight;
-                predecessor[neighbors[k]] = v;
-
-                // Colore os vértices sendo comparados
-                await new Promise(p => setTimeout(p, 1000));
-                ggraph.nodes[v].hue = 320;
-                ggraph.nodes[neighbors[k]].hue = 320;
-                ggraph.edges[edg].hue = 320;
+        vis[u] = true;
+        for(let e=0; e < adj[u].length; e++){
+            let v = adj[u][e];
+            let c = parseInt(ggraph.edges[ggraph.get_edge_index(u, v)].label);
+            if(distance[v] > distance[u] + c){
+                distance[v] = distance[u] + c;
             }
-
-            await new Promise(p => setTimeout(p, 1000));
-            ggraph.nodes[v].hue = 120;
-            ggraph.nodes[neighbors[k]].hue = 120;
-            ggraph.edges[edg].hue = 0;
-            //console.log(ggraph.edges[edg]);
         }
     }
-
-    //console.log(predecessor);
-
-    // Recupera o caminho final
-    var path = [];
-
-    for (let v = target; v != source; v = predecessor[v]) {
-        path.unshift(v);
+    // minimum distance from source to every node now stored in distance array.
+    for(let i=0;i<n;i++){
+        ggraph.nodes[i].label = distance[i].toString();
     }
-
-    path.unshift(source);
-    console.log(path);
-
-    //var edge;
-    for (let i = 1; i < path.length; i++) {
-        ggraph.nodes[path[i]].hue = 120;
-        edge = ggraph.get_edge_index(path[i - 1], path[i]);
-        ggraph.edges[edge].hue = 120;
-    }
-    return
 }
 
 
-async function Bellman(source, target) {
-    // m = ?
-    // e[j].a = u ; e[j].b = v
-    // d[e[j].a] = peso do vértice u em e
-
-    var visitados = [];
-    var predecessor = [];
-    var x;
-
-    for (let i = 0; i < ggraph.nodes.length; i++) {
-        ggraph.nodes[i].weight = Infinity;
-        predecessor[i] = -1;
-        visitados[i] = false;
-    }
-
-    ggraph.nodes[0].weight = 0;
-    /* 
-        for(;;) {
-            var any = false;
-    
-            for(let j = 0; j < ggraph.edges.length; j++) {
-    
-                if(ggraph.edges[j].label == "" ) {
-                    console.log('sem peso');
-                }
-                else {
-                    console.log('com peso. peso = ' + ggraph.edges[j].label);
-                    ggraph.edges[j].weight = parseInt(ggraph.edges[j].label, 10);
-                } 
-                
-    
-                if(ggraph.edges[j].u.weight < Infinity) {
-                    if(ggraph.edges[j].v.weight > (ggraph.edges[j].u.weight + ggraph.edges[j].weight)) {
-                        ggraph.edges[j].v.weight = ggraph.edges[j].u.weight + ggraph.edges[j].weight;
-                        predecessor[ggraph.nodes.indexOf(ggraph.edges[j].v)] = ggraph.nodes.indexOf(ggraph.edges[j].u);
-                        
-                        console.log(ggraph.edges[j].u);
-                        console.log(ggraph.edges[j].v);
-                        
-                        any = true;
-                    }
-                }
-    
-                //console.log(ggraph.edges[j].u);
-            }
-            
-    
-            if(!any) break;
-        } */
-
-    for (let i = 0; i < ggraph.nodes.length; i++) {
-        x = -1;
-        for (let j = 0; j < ggraph.edges.length; j++) {
-            if (ggraph.edges[j].label == "") {
-                continue;
-            }
-            else {
-                ggraph.edges[j].weight = parseInt(ggraph.edges[j].label, 10);
-            }
-
-            if (ggraph.edges[j].u.weight < Infinity) {
-
-                if (ggraph.edges[j].v.weight > (ggraph.edges[j].u.weight + ggraph.edges[j].weight)) {
-                    ggraph.edges[j].v.weight = ggraph.edges[j].u.weight + ggraph.edges[j].weight;
-                    predecessor[ggraph.nodes.indexOf(ggraph.edges[j].v)] = ggraph.nodes.indexOf(ggraph.edges[j].u);
-                    x = ggraph.nodes.indexOf(ggraph.edges[j].v);
-
-                    /* console.log(ggraph.edges[j].u);
-                    console.log(ggraph.edges[j].v); */
-
-                }
-            }
-
-        }
-    }
-
-    // Redefine os pesos das arestas 
-    var aux;
-    for (let j = 0; j < ggraph.edges.length; j++) {
-        aux = ggraph.edges[j].weight + ggraph.edges[j].u.weight - ggraph.edges[j].v.weight;
-        ggraph.edges[j].weight = aux;
-        ggraph.edges[j].label = aux;
-    }
-
-    if (x == -1) {
-        console.log("No negative cycle found.");
-    }
-    else {
-        console.log("Negative cycle: ");
-    }
-
-
-    console.log(ggraph.edges);
-    console.log(predecessor);
-
-    Dijkstra(source, target);
-    return
-    var path = [];
-
-    for (let v = target; v != source; v = predecessor[v]) {
-        path.unshift(v);
-    }
-
-    path.unshift(source);
-    console.log(path);
-
-    var edge;
-    for (let i = 1; i < path.length; i++) {
-        ggraph.nodes[path[i]].hue = 120;
-        edge = ggraph.get_edge_index(path[i - 1], path[i]);
-        ggraph.edges[edge].hue = 120;
-    }
-    //Dijkstra(source, target);
-}
